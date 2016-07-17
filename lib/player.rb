2b30083dfd5dcf2808fccca076937a3ad33f06f2
PlanetEfficacy <jspevack@gmail.com>
2016-07-17 17:18:46 -0600

    player can place ship, but still needs validation

--- lib/player.rb
+++ lib/player.rb
@@ -1,14 +1,17 @@
+require './lib/ship'
 class Player
   attr_reader :shots_fired,
               :winner,
               :known_spaces,
-              :ships
+              :fleet
 
   def initialize(ship_types)
     @shots_fired = 0
     @winner = false
     @known_spaces = []
     @ships = ship_types
+    @fleet = []
+    assemble_the_fleet
   end
 
   def fire
@@ -20,7 +23,16 @@ class Player
   end
 
   def ship_count
-    @ships.length
+    @fleet.length
+  end
+
+  def assemble_the_fleet
+    @fleet = @ships.map { |length| Ship.new(length) }
+  end
+
+  def place_ship(ship, start_space, end_space)
+    #NEEDS VALIDATION
+    ship.place(start_space, end_space)
   end
 
 
--- test/player_test.rb
+++ test/player_test.rb
@@ -71,22 +71,67 @@ class PlayerTest < Minitest::Test
     assert_equal 4, p3.ship_count
   end
 
-  def test_player_starts_with_given_type_of_ships
+  def test_player_instantiates_ships_upon_instantiation
     p1 = Player.new([2, 3])
     p2 = Player.new([2, 3, 4])
     p3 = Player.new([2, 3, 4, 5])
 
-    assert_equal [2, 3], p1.ships
-    assert_equal [2, 3, 4], p2.ships
-    assert_equal [2, 3, 4, 5], p3.ships
+    actual1 = p1.assemble_the_fleet.map do |ship|
+      [ship.class, ship.length]
+    end
+    expected1 = [[Ship, 2], [Ship, 3]]
+
+    actual2 = p2.assemble_the_fleet.map do |ship|
+      [ship.class, ship.length]
+    end
+    expected2 = [[Ship, 2], [Ship, 3], [Ship, 4]]
+
+    actual3 = p3.assemble_the_fleet.map do |ship|
+      [ship.class, ship.length]
+    end
+    expected3 = [[Ship, 2], [Ship, 3], [Ship, 4], [Ship, 5]]
+
+    assert_equal expected1, actual1
+    assert_equal expected2, actual2
+    assert_equal expected3, actual3
+
   end
-  # def test_player_can_place_ship
-  #   p1 = Player.new
-  #
-  # end
-  #
-  # @beginner = {"length"=>4, "ships"=>[2, 3]}
-  # @intermediate = {"length"=>8, "ships"=>[2, 3, 4]}
-  # @advanced = {"length"=>12, "ships"=>[2, 3, 4, 5]}
+
+
+  def test_player_can_place_ship
+    p1 = Player.new([2, 3])
+    p2 = Player.new([2, 3, 4])
+    p3 = Player.new([2, 3, 4, 5])
+    expected1_start = "A1"
+    expected1_end = "B1"
+    expected2_start = "C5"
+    expected2_end = "C7"
+    expected3_start = "K12"
+    expected3_end = "K7"
+
+    ship1a = p1.fleet[0]
+    p1.place_ship(ship1a, "A1", "B1")
+    actual1_start = ship1a.start_space
+    actual1_end = ship1a.end_space
+
+    ship2a = p2.fleet[1]
+    p2.place_ship(ship2a, "C5", "C7")
+    actual2_start = ship2a.start_space
+    actual2_end = ship2a.end_space
+
+    ship3a = p3.fleet[3]
+    p3.place_ship(ship3a, "K12", "K7")
+    actual3_start = ship3a.start_space
+    actual3_end = ship3a.end_space
+
+    assert_equal expected1_start, actual1_start
+    assert_equal expected1_end, actual1_end
+
+    assert_equal expected2_start, actual2_start
+    assert_equal expected2_end, actual2_end
+
+    assert_equal expected3_start, actual3_start
+    assert_equal expected3_end, actual3_end
+  end
 
 end
