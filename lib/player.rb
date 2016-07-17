f18df4a324114b343574dd7ca6f6dd5b8abea07d
PlanetEfficacy <jspevack@gmail.com>
2016-07-17 11:37:11 -0600

    Player does not know spaces and is not the winner when instantiated

--- lib/player.rb
+++ lib/player.rb
@@ -1,6 +1,11 @@
 class Player
-  attr_reader :shots_fired
+  attr_reader :shots_fired,
+              :winner,
+              :known_spaces
+              
   def initialize
-    @shots_fired = 0 
+    @shots_fired = 0
+    @winner = false
+    @known_spaces = []
   end
 end
--- test/player_test.rb
+++ test/player_test.rb
@@ -6,4 +6,14 @@ class PlayerTest < Minitest::Test
     p1 = Player.new
     assert_equal 0, p1.shots_fired
   end
+
+  def test_player_starts_not_winning_the_game
+    p1 = Player.new
+    refute p1.winner
+  end
+
+  def test_player_starts_not_knowing_any_spaces
+    p1 = Player.new
+    assert_equal [], p1.known_spaces
+  end
 end
