3769e45b70bb9dc24ba5046f10f3c04c356ab9b5
PlanetEfficacy <jspevack@gmail.com>
2016-07-19 21:40:43 -0600

    AI can pick random unattacked spot and random unoccupied spot

--- lib/ai_player.rb
+++ lib/ai_player.rb
@@ -1,6 +1,39 @@
 require './lib/player'
 
 class AiPlayer < Player
-  # def autopick(board)
-  # end
+  def pick(board)
+    board.create_space_names.sample
+  end
+
+  def pick_exhaustive(board)
+    pick_history = []
+    board_spaces = board.create_space_names
+    until all_spaces_have_been_picked(board_spaces) do
+      this_pick = pick(board)
+      if not_picked_before?(this_pick, pick_history)
+        board_spaces.delete(this_pick)
+        pick_history << this_pick
+      end
+    end
+    pick_history
+  end
+
+  def all_spaces_have_been_picked(board_spaces)
+    board_spaces.length == 0
+  end
+
+  def not_picked_before?(this_pick, pick_history)
+    !pick_history.include?(this_pick)
+  end
+
+  def pick_unoccupied(board)
+     picks = pick_exhaustive(board)
+     picks.find { |coordinate| !board.space_occupied?(coordinate) }
+  end
+
+  def pick_unattacked(board)
+    picks = pick_exhaustive(board)
+    picks.find { |coordinate| !board.space_attacked?(coordinate)}
+  end
+
 end
--- test/ai_player_test.rb
+++ test/ai_player_test.rb
@@ -4,17 +4,84 @@ require './lib/ai_player'
 class AiPlayerTest < Minitest::Test
 
   def test_ai_can_pick_random_spot
-    skip
+
     ai = AiPlayer.new([2, 3])
     board = Board.new(4)
     coordinates = ai.pick(board)
     assert board.contains?(coordinates)
   end
-# def test_player_can_autopick_spot
-#   p1 = Player.new([2, 3])
-#   board = Board.new(12)
-#
-#   assert_equal board.val
-# end
 
+  def test_ai_can_pick_all_spots_on_board_without_repeating
+    ai = AiPlayer.new([2,3])
+    board = Board.new(4)
+    pick_history = ai.pick_exhaustive(board).sort
+    coordinates = board.create_space_names.sort
+
+    assert_equal coordinates, pick_history
+  end
+
+  def test_it_knows_if_all_spaces_have_been_picked
+    ai = AiPlayer.new([2,3])
+    board = Board.new(4)
+    board_spaces = board.create_space_names
+
+    assert ai.all_spaces_have_been_picked([])
+    refute ai.all_spaces_have_been_picked(board_spaces)
+  end
+
+  def test_it_knows_if_a_space_has_been_picked_before
+    ai = AiPlayer.new([2,3])
+    pick_history = ["A1","A2","A3"]
+
+    refute ai.not_picked_before?("A1", pick_history)
+    assert ai.not_picked_before?("B1", pick_history)
+  end
+
+  def test_ai_can_pick_random_unoccupied_spot_on_trivial_board
+    ai = AiPlayer.new([2,3])
+    board = Board.new(1)
+    expected = "A1"
+    actual = ai.pick_unoccupied(board)
+
+    assert_equal expected, actual
+  end
+
+  def test_ai_can_pick_random_unoccupied_spot
+    ai = AiPlayer.new([2,3])
+    board = Board.new(4)
+    expected = "D4"
+    board.set_spaces_occupied("A1", "A4")
+    board.set_spaces_occupied("B1", "B4")
+    board.set_spaces_occupied("C1", "C4")
+    board.set_spaces_occupied("D1", "D3")
+    actual = ai.pick_unoccupied(board)
+
+    assert_equal expected, actual
+  end
+
+  def test_ai_can_pick_random_unattacked_spot
+    ai = AiPlayer.new([2,3])
+    board = Board.new(4)
+    expected = "C2"
+    board.set_space_attacked("A1")
+    board.set_space_attacked("A2")
+    board.set_space_attacked("A3")
+    board.set_space_attacked("A4")
+    board.set_space_attacked("B1")
+    board.set_space_attacked("B2")
+    board.set_space_attacked("B3")
+    board.set_space_attacked("B4")
+    board.set_space_attacked("C1")
+    board.set_space_attacked("C3")
+    board.set_space_attacked("C4")
+    board.set_space_attacked("D1")
+    board.set_space_attacked("D2")
+    board.set_space_attacked("D3")
+    board.set_space_attacked("D4")
+
+    actual = ai.pick_unattacked(board)
+
+    assert_equal expected, actual
+  end
+  
 end
